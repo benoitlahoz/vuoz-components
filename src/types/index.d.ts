e4c515d68e96314ddb4d9c7eedfc9192a20ed5bc
oz- <benoit.lahoz@carasuelo.org>
2021-08-20 21:19:41 +0200

    Auto update.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,28 +1,29 @@
 import { VueConstructor } from "vue";
 
-export const VuozButton: VueConstructor
-export const VuozCheckbox: VueConstructor
-export const VuozInput: VueConstructor
-export const VuozSignIn: VueConstructor
-export const VuozLoadingIcon: VueConstructor
-export const VuozMenu: VueConstructor
-export const VuozTabs: VueConstructor
-export const VuozLoader: VueConstructor
-export const VuozImage: VueConstructor
-export const VuozCropper: VueConstructor
-export const VuozConsole: VueConstructor
-export const VuozConsoleItem: VueConstructor
-export const VuozToolbar: VueConstructor
-export const VuozSplitView: VueConstructor
-export const VuozTitleBar: VueConstructor
-export const VuozTable: VueConstructor
-export const VuozTableHeader: VueConstructor
-export const VuozTableRow: VueConstructor
-export const VuozPreferencesItem: VueConstructor
-export const VuozPreferencesPanel: VueConstructor
-export const VuozMonitorView: VueConstructor
-export const VuozWhiteBoard: VueConstructor
+export const VuozButton: VueConstructor;
+export const VuozCheckbox: VueConstructor;
+export const VuozInput: VueConstructor;
+export const VuozSignIn: VueConstructor;
+export const VuozLoadingIcon: VueConstructor;
+export const VuozMenu: VueConstructor;
+export const VuozTabs: VueConstructor;
+export const VuozLoader: VueConstructor;
+export const VuozImage: VueConstructor;
+export const VuozCropper: VueConstructor;
+export const VuozConsole: VueConstructor;
+export const VuozConsoleItem: VueConstructor;
+export const VuozToolbar: VueConstructor;
+export const VuozSplitView: VueConstructor;
+export const VuozTitleBar: VueConstructor;
+export const VuozTable: VueConstructor;
+export const VuozTableHeader: VueConstructor;
+export const VuozTableRow: VueConstructor;
+export const VuozPreferencesItem: VueConstructor;
+export const VuozPreferencesPanel: VueConstructor;
+export const VuozMonitorView: VueConstructor;
+export const VuozWhiteBoard: VueConstructor;
+export const VuozUserCell: VueConstructor;
 
-export const VuozFormatterMixin: VueConstructor
+export const VuozFormatterMixin: VueConstructor;
 
-export { CropperOptions } from '@/components/cropper/types'
+export { CropperOptions } from "@/components/cropper/types";
